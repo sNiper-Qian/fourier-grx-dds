45fbbdd7cd3bbb4a95aa02708b54544d22173229
Cheng Qian <cheng.qian@tum.de>
2024-11-27 20:09:33 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ packages = find_packages(include=['fourier_grx_dds', 'fourier_grx_dds.*'])
 print("Packages found:", packages)
 setup(
     name='fourier-grx-dds',
-    version='0.1.9_beta',
+    version='0.2.1_alpha',
     packages=find_packages(include=['fourier_grx_dds', 'fourier_grx_dds.*']),
     include_package_data=True,
     package_data={
